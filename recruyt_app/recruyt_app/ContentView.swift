4b0bd6e7be8eb95fcb52218aa60c867d6df35aa0
Adam <adamdkohler@gmail.com>
2021-08-09 18:42:59 -0700

    xcode init

---
+++ recruyt_app/recruyt_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recruyt_app
+//
+//  Created by Student Account on 8/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ recruyt_app/recruyt_app/recruyt_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recruyt_appApp.swift
+//  recruyt_app
+//
+//  Created by Student Account on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct recruyt_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
